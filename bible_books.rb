a74df63e928a3166df97e4e8e29460764e06f03a
naysayer <joshuagregoryshuster@gmail.com>
2012-10-07 02:29:27 -0700

    made changes

--- bible_books.rb
+++ bible_books.rb
@@ -1,22 +1,41 @@
-@bibleBooks = []
-@passage = []
-File.read("passage.txt").scan(/\w+/) do |word|
-  @passage << word.downcase.chomp
-end
-File.read("bible_books.txt").each_line do |line|
-  @bibleBooks << line.downcase.chomp
-end
-
-@passage.each do |x|
-  @bibleBooks.each do |y|
-    if x == y
-      puts x
-      puts y
-    else
-      puts "failure"
+class BibleBooks
+  @@bibleBooks = []
+  @@passage = []
+  @@passage_master = []
+  @@bibleBooks_master = []
+  
+  File.read("passage.txt").scan(/\w+/) do |word|
+    @@passage << word.downcase.chomp
+  end
+  File.read("bible_books.txt").each_line do |line|
+    @@bibleBooks << line.downcase.chomp
+  end
+  
+  @@bibleBooks.each do |x|
+    @@bibleBooks_master << x.split(//).to_a
+  end
+  
+  @@passage.each do |x|
+    @@passage_master << x.split(//).to_a
+  end
+  
+  @@bibleBooks_master.each.each do |x|
+    @@passage_master.each.each do |y|
+      if x == y
+        puts "candy"
+      end
     end
   end
+  
 end
+
+#  @@bibleBooks.each do |x|
+#    @@passage.each do |y|
+#     if x == y
+#       puts x
+#     end
+#   end
+# end
 
 # @passage.each do |x|
 #   candy = x.to_s.split(//)
